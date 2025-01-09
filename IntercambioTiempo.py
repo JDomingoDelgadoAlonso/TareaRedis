57182e20229539fa2889115fd00b5da9773ef391
José Domingo Delgado Alonso <jose.delgado.alonso.al@iespoligonosur.org>
2025-01-09 16:07:31 +0100

    priemrcomit

---
+++ IntercambioTiempo.py
@@ -0,0 +1,35 @@
+# 1 - Crear registros clave-valor
+
+# 2 - Obtener y mostrar el número de claves registradas
+
+# 3 - Obtener y mostrar un registro en base a una clave
+
+# 4 - Actualizar el valor de una clave y mostrar el nuevo valor(0.5 puntos)
+
+# 5 - Eliminar una clave-valor y mostrar la clave y el valor eliminado(0.5 puntos)
+
+# 6 - Obtener y mostrar todas las claves guardadas (0.5 puntos)
+
+# 7 - Obtener y mostrar todos los valores guardados(0.5 puntos)
+
+# 8 - Obtener y mostrar varios registros con una clave con un patrón en común usando * (0.5 puntos)
+
+# 9 - Obtener y mostrar varios registros con una clave con un patrón en común usando [] (0.5 puntos)
+
+# 10 - Obtener y mostrar varios registros con una clave con un patrón en común usando ? (0.5 puntos)
+
+# 11 - Obtener y mostrar varios registros y filtrarlos por un valor en concreto. (0.5 puntos)
+
+# 12 - Actualizar una serie de registros en base a un filtro (por ejemplo aumentar su valor en 1 )(0.5 puntos)
+
+# 13 - Eliminar una serie de registros en base a un filtro (0.5 puntos)
+
+# 14 - Crear una estructura en JSON de array de los datos que vayais a almacenar(0.5 puntos)
+
+# 15 - Realizar un filtro por cada atributo de la estructura JSON anterior (0.5 puntos)
+
+# 16 - Crear una lista en Redis (0.5 puntos)
+
+# 17 - Obtener elementos de una lista con un filtro en concreto(0.5 puntos)
+
+# 18 - En Redis hay otras formas de almacenar datos: Set, Hashes, SortedSet,Streams, Geopatial, Bitmaps, Bitfields,Probabilistic y Time Series. Elige dos de estos tipos, y crea una función que los guarde en la base de datos y otra que los obtenga. (1.5 puntos)
---
+++ main.py
@@ -0,0 +1,3 @@
+from conexion import obtener_conexion_redis
+# Obtenemos la conexión a Redis
+baseDatosRedis = obtener_conexion_redis()
